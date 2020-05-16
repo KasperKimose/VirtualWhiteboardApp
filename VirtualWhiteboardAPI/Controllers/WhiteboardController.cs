193c48f5d0f38166e7775d8dca2b09923ce2526a
Kasper Kimose Rasmussen <kkr@2BM.DK>
2020-05-16 10:45:23 +0200

    Adds inital whiteboard controller and service

---
+++ VirtualWhiteboardAPI/Controllers/WhiteboardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VirtualWhiteboardAPI.Services;
+
+namespace VirtualWhiteboardAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhiteboardController : ControllerBase
+    {
+        private readonly IWhiteboardService _whiteboardService;
+
+        public WhiteboardController(IWhiteboardService whiteboardService)
+        {
+            _whiteboardService = whiteboardService;
+        }
+    }
+}
---
+++ VirtualWhiteboardAPI/Services/WhiteboardService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualWhiteboardAPI.Models.DataAccess;
+
+namespace VirtualWhiteboardAPI.Services
+{
+    public class WhiteboardService : IWhiteboardService
+    {
+        private readonly VirtualWhiteboardContext _context;
+
+        public WhiteboardService(VirtualWhiteboardContext context)
+        {
+            _context = context;
+        }
+    }
+}
